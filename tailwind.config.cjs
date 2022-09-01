fc1c084bea28a971f656ee094ed46fa2dcf6196b
Javier Reyes <javier.reys@gmail.com>
2022-09-01 01:30:11 -0400

    feat: add more options to tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
+const plugin = require("tailwindcss/plugin");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          highlight: (value) => ({
+            boxShadow: `inset 0 1px 0 0 ${value}`,
+          }),
+        },
+        {
+          values: flattenColorPalette(theme("backgroundColor")),
+          type: "color",
+        }
+      );
+    }),
+  ],
 };
