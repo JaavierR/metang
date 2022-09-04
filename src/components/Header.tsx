23de4f49e12fe7612e546259853aa5d716b10cc2
Javier Reyes <javier.reys@gmail.com>
2022-09-03 20:22:48 -0400

    feat: add blur effect to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
   return (
     <header
       className={clsx(
-        "sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8",
+        "sticky top-0 z-50 flex flex-wrap items-center justify-between bg-white/30 backdrop-blur-md px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 dark:shadow-none sm:px-6 lg:px-8",
         {
           "dark:bg-slate-900/95 dark:backdrop-blur dark:[@supports(backdrop-filter:blur(0))]:bg-slate-900/75":
             isScrolled,
